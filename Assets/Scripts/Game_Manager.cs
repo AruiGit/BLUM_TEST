420c3387aeb8fae3f9adc4db0f7aa01977bec572
Arui <41237117+AruiGit@users.noreply.github.com>
2021-06-14 16:23:51 +0200

    Animations, UI, Player
    Added player animations and animator
    UI- Added player healthPoints
    Player- added Attack method, detection of ground, movement upgrades

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game_Manager : MonoBehaviour
 {
+
+    [SerializeField] Text cointText;
+    [SerializeField] Image[] healthBars;
+    [SerializeField] Sprite fullHeart;
+    [SerializeField] Sprite emptyHeart;
+
+    Player_Controler player;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player").GetComponent<Player_Controler>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CoinUiUpdate();
+        HearthUiUpdate();
+    }
+
+    void CoinUiUpdate()
+    {
+        cointText.text = "Coins: " + player.GetCoins();
+    }
+
+    void HearthUiUpdate()
+    {
+        int playerHealth = player.GetHealth();
+
+        if (playerHealth == 3)
+        {
+            foreach(Image healthBar in healthBars)
+            {
+                healthBar.sprite = fullHeart;
+            }
+        }
+        if (playerHealth == 2)
+        {
+            healthBars[2].sprite = emptyHeart;
+
+            healthBars[1].sprite = fullHeart;
+            healthBars[0].sprite = fullHeart;
+        }
+        if (playerHealth == 1)
+        {
+            healthBars[2].sprite = emptyHeart;
+            healthBars[1].sprite = emptyHeart;
+
+            healthBars[0].sprite = fullHeart;
+        }
+        if (playerHealth == 0)
+        {
+            foreach (Image healthBar in healthBars)
+            {
+                healthBar.sprite = emptyHeart;
+            }
+        }
     }
 }
--- Assets/Scripts/Player_Controler.cs
+++ Assets/Scripts/Player_Controler.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class Player_Controler : MonoBehaviour
 {
+    //Sprite and animations
+    SpriteRenderer sprite;
+    Animator playerAnimator;
+
     //Movement
     float horizontalInput;
     int speed = 5;
     float jumpHight = 7;
     Rigidbody2D rb;
+    bool isGrounded;
+    bool isPreparingToJump = false;
 
     //Stats
-    int healthPoints = 2;
+    [SerializeField]int healthPoints = 3;
     int money = 0;
     int damage = 1;
+    float attackSpeed = 1; //attacksPerSec
+
+    //Attack
+    [SerializeField]Transform attackPosition;
+    [SerializeField] float attackRange = 0.5f;
+    bool canAttack = true;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,28 +40,127 @@ public class Player_Controler : MonoBehaviour
     {
         Movement();
         Jump();
+        Attack();
     }
 
     void Movement()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        Vector2 direction = new Vector2(horizontalInput, 0);
-        if (horizontalInput != 0)
+        if (isPreparingToJump == false)
         {
-            transform.Translate(direction * speed * Time.deltaTime);
+            horizontalInput = Input.GetAxis("Horizontal");
+            Vector2 direction = new Vector2(horizontalInput, 0);
+            playerAnimator.SetBool("isRuning", true);
+            if (horizontalInput != 0)
+            {
+                transform.Translate(direction * speed * Time.deltaTime);
+            }
+            if (horizontalInput < 0)
+            {
+                sprite.flipX = true;
+            }
+            else
+            {
+                sprite.flipX = false;
+            }
+            
+        }
+        if (horizontalInput == 0)
+        {
+            playerAnimator.SetBool("isRuning", false);
         }
     }
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isGrounded == true)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpHight);
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpHight);
+                playerAnimator.ResetTrigger("prepToJump");
+                playerAnimator.SetBool("isJumping", true);
+                isGrounded = false;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                playerAnimator.SetTrigger("prepToJump");
+                isPreparingToJump = true;
+            }
+            else
+            {
+                isPreparingToJump = false;
+            }
+
+            if (isGrounded == true)
+            {
+                playerAnimator.SetBool("isJumping", false);
+            }
         }
-        
+
+        if (rb.velocity.y < 0)
+        {
+            playerAnimator.SetBool("isFalling", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("isFalling", false);
+        }
+
+    }
+
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canAttack==true)
+        {
+            
+           Collider2D[] enemiesArrey = Physics2D.OverlapCircleAll(attackPosition.position, attackRange);
+            playerAnimator.SetTrigger("isAttacking");
+            canAttack = false;
+            StartCoroutine(attackCooldown());
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPosition == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPosition.position, attackRange);
     }
 
     public void AddCoints(int value)
     {
         money += value;
+    }
+    public int GetCoins()
+    {
+        return money;
+    }
+
+    public void ChangeHealth(int value)
+    {
+        healthPoints += value;
+    }
+    public int GetHealth()
+    {
+        return healthPoints;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    IEnumerator attackCooldown()
+    {
+        yield return new WaitForSeconds(1 / attackSpeed);
+        canAttack = true;
+        playerAnimator.ResetTrigger("isAttacking");
     }
 }
